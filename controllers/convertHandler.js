60c03ba2d4b6e6b413a632648f779d68d3e6da03
Ali Ebrahimi <aliebrahimi2150@gmail.com>
2023-06-02 23:51:42 +0330

    feat: write ConvertHandler controller logic

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,113 @@
-function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
-  };
+class Unit {
+  constructor({ name, counterpart, symbol, ratio }) {
+    this.name = name;
+    this.counterpart = counterpart;
+    this.symbol = symbol;
+    this.ratio = ratio;
+  }
+}
 
-  this.spellOutUnit = function(unit) {
+const UNITS = [
+  new Unit({
+    name: 'liters',
+    counterpart: 'gallons',
+    ratio: 1 / 3.78541,
+    symbol: 'L',
+  }),
+  new Unit({
+    name: 'gallons',
+    counterpart: 'liters',
+    ratio: 3.78541,
+    symbol: 'gal',
+  }),
+  new Unit({
+    name: 'miles',
+    counterpart: 'kilometers',
+    ratio: 1.60934,
+    symbol: 'mi',
+  }),
+  new Unit({
+    name: 'kilometers',
+    counterpart: 'miles',
+    ratio: 1 / 1.60934,
+    symbol: 'km',
+  }),
+  new Unit({
+    name: 'pounds',
+    counterpart: 'kilogerams',
+    ratio: 0.453592,
+    symbol: 'lbs',
+  }),
+  new Unit({
+    name: 'kilogerams',
+    counterpart: 'pounds',
+    ratio: 1 / 0.453592,
+    symbol: 'kg',
+  }),
+];
+
+class ConvertError extends Error {}
+
+function ConvertHandler() {
+  this.convert = function (initNum, initUnitSymbol) {
+    initNum = initNum || '1';
+    if (!this.isNumValid(initNum) && !this.isUnitValid(initUnitSymbol)) {
+      throw new ConvertError('invalid number and unit');
+    } else if (!this.isNumValid(initNum)) {
+      throw new ConvertError('invalid number');
+    } else if (!this.isUnitValid(initUnitSymbol)) {
+      throw new ConvertError('invalid unit');
+    } else {
+      const initUnit = this.parseUnit(initUnitSymbol);
+      const returnNum = +(initNum * initUnit.ratio).toFixed(5);
+      const returnUnit = UNITS.find(
+        (unit) => unit.name === initUnit.counterpart
+      );
+      let result = {
+        initNum: this.parseNum(initNum),
+        initUnit: initUnit.symbol,
+        returnUnit: returnUnit.symbol,
+        returnNum: returnNum,
+        string: `${initNum} ${initUnit.name} converts to ${returnNum} ${returnUnit.name}`,
+      };
+      return result;
+    }
+  };
+  this.isNumValid = function (num) {
+    const splittedOnSlash = num.split('/');
+    if (splittedOnSlash.length > 2) {
+      return false;
+    }
+    return true;
+  };
+  this.isUnitValid = function (unitSymbol) {
+    if (
+      !unitSymbol ||
+      UNITS.findIndex(
+        (unit) => unit.symbol.toLowerCase() === unitSymbol.toLowerCase()
+      ) < 0
+    ) {
+      return false;
+    }
+    return true;
+  };
+  this.parseUnit = function (input) {
     let result;
-    
+    result = UNITS.find(
+      (unit) => unit.symbol.toLowerCase() === input.toLowerCase()
+    );
     return result;
   };
-  
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
+  this.parseNum = function (input) {
     let result;
-    
+    const splittedOnSlash = input.split('/');
+    if (splittedOnSlash.length > 1) {
+      result = parseFloat(eval(input));
+    } else {
+      result = parseFloat(input);
+    }
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
-  };
-  
 }
 
 module.exports = ConvertHandler;
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,70 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Functional Tests', function () {
+  suiteTeardown(() => {
+    requester.close();
+  });
 
+  test('Convert a valid input such as 10L: GET request to /api/convert', (done) => {
+    requester
+      .get('/api/convert')
+      .query({
+        input: '10L',
+      })
+      .end((err, res) => {
+        assert.isNull(err, 'err should be null');
+        assert.strictEqual(res.status, 200);
+        assert.strictEqual(res.body.returnNum, 2.64172);
+        done();
+      });
+  });
+  test('Convert an invalid input such as 32g: GET request to /api/convert', (done) => {
+    requester
+      .get('/api/convert')
+      .query({
+        input: '32g',
+      })
+      .end((err, res) => {
+        assert.notExists(err);
+        assert.strictEqual(res.body, 'invalid unit');
+        done();
+      });
+  });
+  test('Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert', (done) => {
+    requester
+      .get('/api/convert')
+      .query({
+        input: '3/7.2/4kg',
+      })
+      .end((err, res) => {
+        assert.notExists(err);
+        assert.strictEqual(res.body, 'invalid number');
+        done();
+      });
+  });
+  test('Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert', (done) => {
+    requester
+      .get('/api/convert')
+      .query({
+        input: '3/7.2/4kilomegagram',
+      })
+      .end((err, res) => {
+        assert.notExists(err);
+        assert.strictEqual(res.body, 'invalid number and unit');
+        done();
+      });
+  });
+  test('Convert with no number such as kg: GET request to /api/convert', (done) => {
+    requester
+      .get('/api/convert')
+      .query({
+        input: 'kg',
+      })
+      .end((err, res) => {
+        assert.notExists(err);
+        assert.strictEqual(res.body.returnNum, 2.20462);
+        done();
+      });
+  });
 });
